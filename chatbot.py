e493965287289fae4f3c0fb64e60165fc9e1c7f2
Divya-T1 <105827624+Divya-T1@users.noreply.github.com>
2024-10-19 15:24:29 -0500

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -2,20 +2,21 @@ import streamlit as st
 def chatbot():
         with st.chat_message("User"):
                 st.write("Hello ✋")
-        # Initialize chat history
+                
+         # Initialize chat history
         if "messages" not in st.session_state:
             st.session_state.messages = []
-
-        # Display chat messages from history on app rerun
-        for message in st.session_state.messages:
-            with st.chat_message(message["role"]):
-                st.markdown(message["content"])
                     
         if prompt := st.chat_input("Message your AI mentor!"):
             # Display user message in chat message container
             with st.chat_message("user"):
                 st.write("What's your name?")
             st.session_state.messages.append({"role": "user","content": prompt})
+
+        # Display chat messages from history on app rerun
+        for message in st.session_state.messages:
+            with st.chat_message(message["role"]):
+                st.markdown(message["content"])
         
         
 

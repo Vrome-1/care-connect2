65ca3a63ce44b4d6326ce1cb2b75d7fca0b80999
Divya-T1 <105827624+Divya-T1@users.noreply.github.com>
2024-10-19 15:12:34 -0500

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -3,20 +3,20 @@ def chatbot():
         with st.chat_message("User"):
                 st.write("Hello ✋")
         # Initialize chat history
-        if "messages" not in st.session_state:
+        '''if "messages" not in st.session_state:
             st.session_state.messages = []
 
         # Display chat messages from history on app rerun
         for message in st.session_state.messages:
             with st.chat_message(message["role"]):
-                st.markdown(message["content"])
+                st.markdown(message["content"])'''
 
         if prompt := st.chat_input("What is up?"):
             # Display user message in chat message container
             with st.chat_message("user"):
-                st.markdown(prompt)
+                st.write(prompt)
             # Add user message to chat history
-            st.session_state.messages.append({"role": "user", "content": prompt})
+           # st.session_state.messages.append({"role": "user", "content": prompt})
         
         
 

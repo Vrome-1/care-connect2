118ea684669b4f8d333c44d938d445276c9c4bcb
Divya-T1 <105827624+Divya-T1@users.noreply.github.com>
2024-10-19 15:34:16 -0500

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -10,11 +10,11 @@ def chatbot():
         if prompt := st.chat_input("Message your AI mentor!"):
             # Display user message in chat message container
             st.session_state.messages.append({"role": "user","content": prompt})
+            message.write("What's your name?")
          # Display chat messages from history on app rerun
         for message in st.session_state.messages:
             with st.chat_message(message["role"]):
                 st.markdown(message["content"])
-        message.write("What's your name?")
         
 
         # # Show title and description.

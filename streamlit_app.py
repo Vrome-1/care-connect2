4030cbfbcb8ae9f7e58bcd7df837dff2824d2534
saumya-cs <35942234+saumya-cs@users.noreply.github.com>
2024-10-19 13:41:02 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,10 +12,7 @@ if page == "Mission":
     st.write("We understand that, in healthcare, mental health is key. Patients go through a lot of stress related to their illnesses - both physical and emotional.");
 
 elif page == "Community Forum":
-    st.title('Our Care Community Forum :heart:');
-    st.subheader("Today's Posts");
-    st.success("Preeti Patel  \nMy story  \n Hi guys, I'm a new Care Connect user and I wanted to share my story today. I've been struggling with my mental health for over a year now, just shortly after my diagnosis of breast cancer. I'm fortunate enough to have a very loving family, but even still I feel alone because they don't know what it's like to go through this. I also feel guilty for not being able to be as strong of a mom as I used to be. Does it ever get better?");
-    st.write("---> Sending lots of love from a breast cancer survivor. I get you girl, and I know it's hard, but we will prevail. Tip for you: keep a positive thoughts journal to help you combat those negative feelings.");
+    forum()
 elif page == "Educational Resources":
     st.subheader("use this")
 elif page == "Mentor Matching":

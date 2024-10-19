67663078c60e54795e1c0dc619dedec67a313c43
Vrome-1 <vaanirometra@yahoo.com>
2024-10-19 16:28:06 -0500

    Update buddy.py

--- buddy.py
+++ buddy.py
@@ -1,1 +1,8 @@
+import streamlit as st
 
+# Function to connect people to buddies
+def buddy():
+  Buddies = {'Cancer': [Alice, Ben, Jaspreet, Donald, June] , 'Depression': [Bernard, Andrew, Melaina, Carl], 'Diabetes': [Delian, Sally], 'Eating Disorder': [Dennis, Julia]}
+  st.h
+  //input disease and name
+  //click the button

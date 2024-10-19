04d07a5d71db6766d21645681b535e0a9b0356e5
saumya-cs <35942234+saumya-cs@users.noreply.github.com>
2024-10-19 14:05:29 -0500

    Update forum.py

--- forum.py
+++ forum.py
@@ -4,6 +4,9 @@ import streamlit as st
 def forum():
     st.title('Our Care Community Forum :heart:');
     st.subheader("Today's Posts");
-    st.success("Preeti Patel  \nMy story  \n Hi guys, I'm a new Care Connect user and I wanted to share my story today. I've been struggling with my mental health for over a year now, just shortly after my diagnosis of breast cancer. I'm fortunate enough to have a very loving family, but even still I feel alone because they don't know what it's like to go through this. I also feel guilty for not being able to be as strong of a mom as I used to be. Does it ever get better?");
+    st.success("Preeti Patel  \n Hi guys, I'm a new Care Connect user and I wanted to share my story today. I've been struggling with my mental health for over a year now, just shortly after my diagnosis of breast cancer. I'm fortunate enough to have a very loving family, but even still I feel alone because they don't know what it's like to go through this. I also feel guilty for not being able to be as strong of a mom as I used to be. Does it ever get better?");
     st.write("---> Sending lots of love from a breast cancer survivor. I get you girl, and I know it's hard, but we will prevail. Tip for you: keep a positive thoughts journal to help you combat those negative feelings.");
-
+    st.subheader("Share Your Story")
+    name = st.text_input("Your Name")
+    title = st.text_input("Title of Your Story")
+    content = st.text_area("Write your story here...")

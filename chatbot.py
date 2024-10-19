61c73c35818cb4153887d428daa72a7bd601e5cf
Divya-T1 <105827624+Divya-T1@users.noreply.github.com>
2024-10-19 15:06:57 -0500

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -10,6 +10,11 @@ def chatbot():
         for message in st.session_state.messages:
             with st.chat_message(message["role"]):
                 st.markdown(message["content"])
+
+        if prompt := st.chat_input("What is your name?"):
+                with st.chat_message("user"):
+                        st.markdown(prompt)
+                st.session_state.messages.append({"role":"user":"content":prompt})
         
         
 

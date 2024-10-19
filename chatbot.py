a7363c1cad895557bd845682d840c7271075a9bd
Divya-T1 <105827624+Divya-T1@users.noreply.github.com>
2024-10-19 14:43:49 -0500

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,7 +1,7 @@
 import streamlit as st
 def chatbot():
-        '''with st.chat_message("User"):
-                st.write("Hello ✋")'''
+        with st.chat_message("User"):
+                st.write("Hello ✋")
         prompt = st.chat_input("How are you feeling today?")
         if prompt:
                 st.write(f"User has sent the following prompt: {prompt}")

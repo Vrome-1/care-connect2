9744b17d3cb2847e8544dd25273126f0c8eb2511
Vrome-1 <vaanirometra@yahoo.com>
2024-10-19 16:58:00 -0500

    Update buddy.py

--- buddy.py
+++ buddy.py
@@ -17,12 +17,11 @@ def buddy():
           buddies_list = Buddies[submit_clicked]
           st.write("Here are some people you can connect with: ")
           st.markdown(
+              ""
               <div style="background-color: #333; padding: 20px; border-radius: 5px; border: 1px solid #ccc;">
               <h3 style="color: #333;">buddies_list</h3>
               <p style="color: #666;">Connect!</p>
               </div>
-              """,
+              "",
             unsafe_allow_html=True
           )
-if __name__ == "__main__":
-    buddy()

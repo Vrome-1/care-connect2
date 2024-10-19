12f0d879b3d2a2165c2508c7bf07b771c01e337a
Divya-T1 <105827624+Divya-T1@users.noreply.github.com>
2024-10-19 14:59:38 -0500

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -6,11 +6,14 @@ def chatbot():
         if prompt:
                 st.write(f"No, you're a dog for life")
 
+        # Initialize chat history
         if "messages" not in st.session_state:
-                st.session_state.messages = []
-        for message in st.session_state.message:
-                with st.chat_message(message["role"]):
-                        st.markdown(message["content"])
+            st.session_state.messages = []
+
+        # Display chat messages from history on app rerun
+        for message in st.session_state.messages:
+            with st.chat_message(message["role"]):
+                st.markdown(message["content"])
         
         
 

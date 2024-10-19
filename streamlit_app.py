b6f7465387eca5c1e83a5cff872fa6577dda5c17
Vrome-1 <vaanirometra@yahoo.com>
2024-10-19 13:48:17 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,16 +3,7 @@ from openai import OpenAI
 
 st.title("ConnectCare")
 st.sidebar.title("Navigation")
-row1, row2, row3, row4 = st.sidebar.row(4)
-with row1:
-    st.button("About")
-with row2:
-    st.button("Educational Resources")
-with row3:
-    st.button("Community Forum")
-with row4:
-    st.button("Mentor Matching")
-page = st.sidebar.selectbox("Choose a page", ["Mission", "Educational Resources", "Community Forum", "Mentor Matching"])
+page = st.sidebar.radio("Choose a page", ["Mission", "Educational Resources", "Community Forum", "Mentor Matching"])
 
 if page == "Mission":
     st.subheader("Welcome!");

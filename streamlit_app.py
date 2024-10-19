3012ace365ea284209125b7377762b43db17603e
Vrome-1 <vaanirometra@yahoo.com>
2024-10-19 12:43:12 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,15 @@
 import streamlit as st
 from openai import OpenAI
 
+st.sidebar.title("Navigation")
+page = st.sidebar.selectbox("Choose a page", ["Mission", "Educational Resources", "Community Forum", "Mentor Matching"])
+
+if page == "Mission":
+    st.subheader("Welcome!");
+    st.write("Welcome to Care Connect! This website is all about support and community - we aim to help patients find and connect with others who understand what they are going through.");
+    st.subheader("Our Motivation");
+    st.write("We understand that, in healthcare, mental health is key. Patients go through a lot of stress related to their illnesses - both physical and emotional.");
+
 # Show title and description.
 st.title("💬 Chatbot")
 st.write(

e059f97bc91a2e8dd2070f7a23d0024209311c9d
Vrome-1 <vaanirometra@yahoo.com>
2024-10-20 12:22:08 -0500

    Update become_buddy.py

--- become_buddy.py
+++ become_buddy.py
@@ -4,3 +4,9 @@ become_buddy() :
   st.title("Become a Health Buddy")
   st.write("Are you looking to make a meaningful connection while supporting others on their health journeys? Join our community of health buddies!")
   st.write("As a health buddy, you'll have the opportunity to connect with individuals who share similar experiences and challenges. Whether you’ve navigated the complexities of cancer, diabetes, depression, or eating disorders, your insights and support can make a real difference in someone else's life.")
+  st.write("Why become a health buddy?")
+  st.markdown("- Share your story")
+  st.markdown("- Join a community")
+  st.markdown("- Make an impact and help others in need")
+  st.write("Ready to take the first step? Sign up today and be a guiding light for someone in need! Together, we can create a supportive network where everyone feels valued and empowered. Join us and be part of something special!")
+  

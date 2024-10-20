f02f0a40732f1d170e772314c4b7ffc39f9c39db
saumya-cs <35942234+saumya-cs@users.noreply.github.com>
2024-10-20 10:26:41 -0500

    hi

--- forum.py
+++ forum.py
@@ -23,7 +23,7 @@ def forum():
         Hi guys, I'm a new Care Connect user and I wanted to share my story today. I've been struggling with my mental health for over a year now, just shortly after my diagnosis of breast cancer. I'm fortunate enough to have a very loving family, but even still I feel alone because they don't know what it's like to go through this. I also feel guilty for not being able to be as strong of a mom as I used to be. Does it ever get better?
     </p></div>""", unsafe_allow_html=True)
 
-     st.markdown("""
+    st.markdown("""
     <div style="background-color:rgba(240, 36, 121, 0.2); padding:10px; border-radius:5px; color: black;">
     <p style="font-size:24px; font-weight:bold; margin:0;">Elaine G</p>
     <p style="margin-top:5px;">

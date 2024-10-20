30ab0c1a0c30151ceda289f04044fcb22a195653
Divya-T1 <105827624+Divya-T1@users.noreply.github.com>
2024-10-20 12:59:15 -0500

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -5,61 +5,29 @@ import os
 import openai
 # Set your OpenAI API key from environment variable
 def chatbot():
-        # Show title and description.
-        st.title("💬 Chatbot")
-        st.write(
-           "Hello!"
-        )
-        #     "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-        #     "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-        #     "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
-        # )
-        
-        #Ask user for their OpenAI API key via `st.text_input`.
-        # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-        # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-        openai_api_key = st.text_input("OpenAI API Key", type="password")
-        if not openai_api_key:
-           st.info("Please add your OpenAI API key to continue.", icon="🗝️")
-        else:
-        
-            # Create an OpenAI client.
-            client = OpenAI(api_key=openai_api_key)
-        
-            # Create a session state variable to store the chat messages. This ensures that the
-            # messages persist across reruns.
-            if "messages" not in st.session_state:
-                st.session_state.messages = []
-        
-            # Display the existing chat messages via `st.chat_message`.
-            for message in st.session_state.messages:
+        message = st.chat_message("User")
+        left_column, right_column = st.columns([3,1])
+        with left_column: 
+                response = random.choice (
+                        [
+                                "Hello!", "Hi There!", "Hi!",
+                        ]
+                )
+                message.write(response)
+         # Initialize chat history
+        if "messages" not in st.session_state:
+            st.session_state.messages = []
+
+        for message in st.session_state.messages:
                 with st.chat_message(message["role"]):
-                    st.markdown(message["content"])
-        
-            # Create a chat input field to allow the user to enter a message. This will display
-            # automatically at the bottom of the page.
-            if prompt := st.chat_input("What is up?"):
-        
-                # Store and display the current prompt.
-                st.session_state.messages.append({"role": "user", "content": prompt})
+                        st.markdown(message["content"])
+                        
+        if prompt := st.chat_input("Message your AI mentor!"):
+                # Display user message in chat message container
                 with st.chat_message("user"):
                     st.markdown(prompt)
-        
-                # Generate a response using the OpenAI API.
-                stream = client.chat.completions.create(
-                    model="gpt-3.5-turbo",
-                    messages=[
-                        {"role": m["role"], "content": m["content"]}
-                        for m in st.session_state.messages
-                    ],
-                    stream=True,
-                )
-        
-                #Stream the response to the chat using `st.write_stream`, then store it in 
-                #session state.
-                with st.chat_message("assistant"):
-                    response = st.write_stream(stream)
-                st.session_state.messages.append({"role": "assistant", "content": response})
+                st.session_state.messages.append({"role": "user","content": prompt})
+
 
 '''st.title("AI Health Mentor")
     st.markdown("Chat with our AI mentor for support and guidance. Your conversations are private and confidential.")
@@ -130,29 +98,6 @@ def chatbot():
         client = OpenAI(
                 api_key = "sk-W2J7u45G4vDH9G90Ab4F88a0OA2gBJvfzjn2qlNftjT3BlbkFJKBEKXSe9UhEYAAjXBtf1AaNbPbNKfih1A088hFrWIA"
         )
-        message = st.chat_message("User")
-        left_column, right_column = st.columns([3,1])
-        with left_column: 
-                response = random.choice (
-                        [
-                                "Hello!", "Hi There!", "Hi!",
-                        ]
-                )
-                message.write(response)
-         # Initialize chat history
-        if "messages" not in st.session_state:
-            st.session_state.messages = []
-            st.session_state["openai_model"] = "gpt-3.5-turbo"
-
-        for message in st.session_state.messages:
-                with st.chat_message(message["role"]):
-                        st.markdown(message["content"])
-                        
-        if prompt := st.chat_input("Message your AI mentor!"):
-                # Display user message in chat message container
-                with st.chat_message("user"):
-                    st.markdown(prompt)
-                st.session_state.messages.append({"role": "user","content": prompt})
                 # Display chat messages from history on app rerun
                 # Display user message
         

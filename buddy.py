65dcc2eb4fbeaaeb1d12f9453f3af6aa65ff1931
Vrome-1 <vaanirometra@yahoo.com>
2024-10-20 11:46:18 -0500

    Update buddy.py

--- buddy.py
+++ buddy.py
@@ -45,9 +45,10 @@ def buddy():
                 """,
                 unsafe_allow_html=True
             )
-      if st.session_state.buddies_list:
-        people_options = st.selectbox('Select a person to connect with:', st.session_state.buddies_list)
+      
+      people_options = st.selectbox('Select a person to connect with:', st.session_state.buddies_list)
+      if people_options:
         connect = st.button("Connect!")
-        if connect:
-            st.write("We sent an email requesting your connection. You will be connected with your buddy soon!")
+          if connect:
+              st.write("We sent an email requesting your connection. You will be connected with your buddy soon!")
 

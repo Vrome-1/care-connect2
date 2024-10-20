49e7b1c283cb61120d7ab7def639aaa711539864
Vrome-1 <vaanirometra@yahoo.com>
2024-10-20 11:43:39 -0500

    Update become_buddy.py

--- become_buddy.py
+++ become_buddy.py
@@ -2,4 +2,5 @@ import streamlit as st
 
 become_buddy() :
   st.title("Become a Health Buddy")
-  st.write
+  st.write("Are you looking to make a meaningful connection while supporting others on their health journeys? Join our community of health buddies!")
+  st.write("As a health buddy, you'll have the opportunity to connect with individuals who share similar experiences and challenges. Whether you’ve navigated the complexities of cancer, diabetes, depression, or eating disorders, your insights and support can make a real difference in someone else's life.")

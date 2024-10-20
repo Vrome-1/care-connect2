74540ea466f646eb09fa3300a4deedbf18a573f2
saumya-cs <35942234+saumya-cs@users.noreply.github.com>
2024-10-20 10:43:02 -0500

    a

--- forum.py
+++ forum.py
@@ -3,21 +3,15 @@ import streamlit as st
 # Function to display the community forum
 def forum():
     # Initialize user stories in session state if not already done
-    st.session_state.user_stories = [
-    {'name': 'Preeti Patel', 'title': 'My Journey', 'content': "Hi guys, I'm a new Care Connect user and I wanted to share my story today. I've been struggling with my mental health for over a year now, just shortly after my diagnosis of breast cancer."},
-    {'name': 'John Doe', 'title': 'Finding Support', 'content': "Hi Preeti, thank you for sharing your story. I’ve been dealing with something similar, and I know how hard it can be. It does get better, and you're stronger than you think."}
-]
-
+     if 'user_stories' not in st.session_state:
+        st.session_state.user_stories = [
+        {'name': 'Preeti Patel', 'title': 'My Journey', 'content': "Hi guys, I'm a new Care Connect user and I wanted to share my story today. I've been struggling with my mental health for over a year now, just shortly after my diagnosis of breast cancer."},
+        {'name': 'Elaine G', 'title': 'Daily Update', 'content': "Good day all. As usual, my daily update. My doctor changed my meds, and now I feel nauseous all the time. Guess it's better than the other ones though because they affect my mood less. I'm really addicted to Words With Friends right now, effectively gets my mind off everything. Anyone want to play with me sometime?"}
+        ]
     st.title('Our Care Community Forum :heart:')
     st.subheader("Today's Posts")
 
     # Sample posts
-    # st.success(
-    #     "Preeti Patel  \nHi guys, I'm a new Care Connect user and I wanted to share my story today. "
-    #     "I've been struggling with my mental health for over a year now, just shortly after my diagnosis of breast cancer. "
-    #     "I'm fortunate enough to have a very loving family, but even still I feel alone because they don't know what it's like to go through this. "
-    #     "I also feel guilty for not being able to be as strong of a mom as I used to be. Does it ever get better?"
-    # )
     # st.markdown("""
     # <div style="background-color:rgba(240, 36, 121, 0.2); padding:10px; border-radius:5px; color: black; margin-bottom:20px;">
     # <p style="font-size:24px; font-weight:bold; margin:0;">Preeti Patel</p>
@@ -62,6 +56,7 @@ def forum():
             })
             st.success("Your story has been added!")  # Confirm submission
             # Reset the input fields
+            st.experimental_rerun()
               # Rerun to update the display
         else:
             st.error("Please fill in all fields.")
